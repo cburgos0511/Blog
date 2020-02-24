145d0b9cfaa9abb49b0d0f622d9a816c3ae12242
cburgos0511 <48486237+cburgos0511@users.noreply.github.com>
2020-02-24 12:39:19 -0600

    about page animation, layout

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -2,6 +2,37 @@ import React from "react"
 import { graphql, Link } from "gatsby"
 import Layout from "../components/layout"
 import Img from "gatsby-image"
+import styled from "styled-components"
+import "./pages.css"
+
+const ImageList = styled.ul`
+  display: flex;
+  flex-flow: row wrap;
+  a {
+    text-decoration: none;
+    color: #000;
+    margin-left: 18px;
+    li {
+      list-style: none;
+      h6 {
+        margin-top: 2vh;
+        font-size: 12px;
+        font-weight: 400;
+        text-transform: uppercase;
+        color: #a3a3a3;
+        letter-spacing: 2px;
+      }
+      h2 {
+        font-family: Roboto;
+        font-weight: 700;
+        font-size: 16px;
+      }
+      .img-wrapper {
+        overflow: hidden;
+      }
+    }
+  }
+`
 
 export const articleQuery = graphql`
   query MyQuery {
@@ -12,12 +43,13 @@ export const articleQuery = graphql`
           id
           published
           title
+          category
           user {
             username
           }
           img {
             childImageSharp {
-              fixed(width: 300, height: 225) {
+              fixed(width: 375, height: 325) {
                 ...GatsbyImageSharpFixed
               }
             }
@@ -31,18 +63,25 @@ export const articleQuery = graphql`
 const Blog = ({ data }) => (
   <Layout>
     <h1 className="header">Blog</h1>
-    <ul>
+    <ImageList>
       {data.allStrapiArticle.edges.map(doc => (
-        <li key={doc.node.id}>
-          <h2>
-            <Link to={`/${doc.node.id}/`}>{doc.node.title}</Link>
-          </h2>
-          <Img fixed={doc.node.img.childImageSharp.fixed} />
-          <p>{doc.node.description}</p>
-          <h6 className='creator'>Written by: {doc.node.user.username}</h6>
-        </li>
+        <Link to={`/${doc.node.id}/`}>
+          <li key={doc.node.id}>
+            <div className="img-wrapper">
+              <Img
+                className="blog-img"
+                fixed={doc.node.img.childImageSharp.fixed}
+              />
+            </div>
+
+            <h6>{doc.node.category}</h6>
+            <h2>{doc.node.title}</h2>
+
+            {/* <p>{doc.node.description}</p> */}
+          </li>
+        </Link>
       ))}
-    </ul>
+    </ImageList>
   </Layout>
 )
 
