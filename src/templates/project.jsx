f3d5ab2c7c14497946440cb108e6946ef4e4cb39
cburgos0511 <48486237+cburgos0511@users.noreply.github.com>
2020-03-02 15:45:09 -0600

    templates projecs css

--- src/templates/project.jsx
+++ src/templates/project.jsx
@@ -3,21 +3,90 @@ import { Link, graphql } from "gatsby"
 import Img from "gatsby-image"
 import Layout from "../components/layout"
 import ReactMarkdown from "react-markdown"
+import styled from "styled-components"
 
+const IndividualProject = styled.div`
+  position: relative;
+  .project-title {
+    width: 60%;
+    margin: 10% auto;
+    text-align: left;
+    p {
+      text-align: left;
+      font-size: 14px;
+      font-weight: 500;
+      color: #525252;
+      text-transform: capitalize;
+    }
+    h6 {
+      position: relative;
+      font-size: 14px;
+      font-weight: 500;
+      text-align: right;
+      width: 20%;
+      margin-left: 80%;
+      &::before {
+        content: "";
+        position: absolute;
+        top: 50%;
+        left: 10%;
+        height: 1px;
+        width: 20px;
+        background: #525252;
+      }
+      a {
+        text-decoration: none;
+        color: #525252;
+      }
+    }
+    h1 {
+      font-family: PT Serif;
+      font-size: 9vh;
+      text-transform: capitalize;
+      font-weight: 400;
+    }
+  }
+  .img-wrap {
+    position: relative;
+    width: 60%;
+    margin: 0 auto 10vh auto;
+  }
+  .project-content {
+    font-family: PT Serif;
+    position: relative;
+    width: 60%;
+    margin: auto;
+    h1 {
+      font-family: PT Serif;
+    }
+    h2 {
+      font-family: PT Serif;
+    }
+    h3 {
+      font-family: PT Serif;
+    }
+    p {
+      font-weight: 300;
+      line-height: 1.5rem;
+    }
+  }
+`
 export const query = graphql`
   query ProjectTemplate($id: String!) {
     strapiProject(id: { eq: $id }) {
       id
       title
       content
+      created_at
+      time
       builder {
         id
         username
       }
       img {
         childImageSharp {
-          fixed(width: 400, height: 325) {
-            ...GatsbyImageSharpFixed
+          fluid(maxWidth: 1000, maxHeight: 950) {
+            ...GatsbyImageSharpFluid
           }
         }
       }
@@ -26,18 +95,26 @@ export const query = graphql`
 `
 const ProjectTemplate = ({ data }) => (
   <Layout>
-    <h1>{data.strapiProject.title}</h1>
-
-    <Img fixed={data.strapiProject.img.childImageSharp.fixed} />
-    <ReactMarkdown
-      source={data.strapiProject.content}
-      transformImageUri={uri =>
-        uri.startsWith("http") ? uri : `${process.env.IMAGE_BASE_URL}${uri}`
-      }
-    />
-    <p>
-      by <Link to="/about">{data.strapiProject.builder.username}</Link>
-    </p>
+    <IndividualProject>
+      <div className="project-title">
+        <p>Duration: {data.strapiProject.time}</p>
+        <h1>{data.strapiProject.title}</h1>
+        <h6>
+          <Link to="/about">{data.strapiProject.builder.username}</Link>
+        </h6>
+      </div>
+      <div className="img-wrap">
+        <Img fluid={data.strapiProject.img.childImageSharp.fluid} />
+      </div>
+      <div className="project-content">
+        <ReactMarkdown
+          source={data.strapiProject.content}
+          transformImageUri={uri =>
+            uri.startsWith("http") ? uri : `${process.env.IMAGE_BASE_URL}${uri}`
+          }
+        />
+      </div>
+    </IndividualProject>
   </Layout>
 )
 
